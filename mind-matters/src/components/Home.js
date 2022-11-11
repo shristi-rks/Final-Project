0e0d9738481f56ec19906b84208d0a6e30198076
Abbey Rut <abbey.rut@gmail.com>
2022-11-11 19:25:28 +0200

    learn-4

--- mind-matters/src/components/Home.js
+++ mind-matters/src/components/Home.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Home = () => {
 
-    const handleClick=()=>{
-        console.log('handle');
+    const handleClick=(e)=>{
+        console.log('handle, wins', e);
     }
-    const handleClickAgain =(name)=>{
-        console.log('hello' + name);
+    const handleClickAgain =(name, e)=>{
+        console.log('hello' + name, e.target);
     }
   return (
     <div className='home'>
         <h2>HomePage</h2>
         <button onClick={handleClick}>Click Me</button>
-        <button onClick={()=>handleClickAgain('marion')}> Click Again</button>
+        <button onClick={(e)=> handleClickAgain(' marion ', e)}> Click Again</button>
 
 
     </div>

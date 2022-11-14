fdd3be9d10cf5ec439b5ba50f2fd64c2f843ed44
Abbey Rut <abbey.rut@gmail.com>
2022-11-14 18:04:54 +0200

    setting reussable functions

--- mind-matters/src/components/Home.js
+++ mind-matters/src/components/Home.js
@@ -1,34 +1,11 @@
 import {useState, useEffect} from 'react'
+import useFetch from '../useFetch';
 import BlogList from './BlogList';
 
 const Home = () =>{
+const{ data: blogs, isPending, error } =useFetch('http://localhost:8000/blogs')
 
-const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending]= useState(true)
-    const [error, setError]= useState (null)  
    
-    useEffect(() =>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-                .then(res =>{
-                    if(!res.ok)
-                    throw Error('could not fetch the data from the resource')
-
-                    
-            return res.json();
-    })
-                .then(data =>{
-                     setBlogs(data)
-                    setIsPending(false)
-                    setError(null)
-    })
-    .catch(err=>{
-        setIsPending(false)
-        setError(err.message)
-    })
-        },1000)
-    },[])
-
 return(
     <div className="home">
         {error && <div>{error}</div>}

dd6161b93b9790b08c3c8b3c54321ac418a6eeac
Abbey Rut <abbey.rut@gmail.com>
2022-11-14 17:16:41 +0200

    send error message

--- mind-matters/src/components/Home.js
+++ mind-matters/src/components/Home.js
@@ -4,7 +4,8 @@ import BlogList from './BlogList';
 const Home = () =>{
 
 const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending]= useState(true)   
+    const [isPending, setIsPending]= useState(true)
+    const [error, setError]= useState (null)  
    
     useEffect(() =>{
         setTimeout(()=>{
@@ -21,13 +22,14 @@ const [blogs, setBlogs] = useState(null);
                     setIsPending(false)
     })
     .catch(err=>{
-        console.log(err.message)
+        setError(err.message)
     })
         },1000)
     },[])
 
 return(
     <div className="home">
+        {error && <div>{error}</div>}
         {isPending && <div>Loading...</div>}
    { blogs && <BlogList blogs={blogs}  title="All Blogs!" /> } 
     

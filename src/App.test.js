a1c344a2ebfbcde206eca64d94d1e8626ccd590f
shristi-rks <shristi.n08@gmail.com>
2022-12-01 13:52:43 +0200

    add yaml file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders mind matters link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/mind matters/i);
   expect(linkElement).toBeInTheDocument();
 });

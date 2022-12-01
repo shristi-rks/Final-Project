0ac82b3a656e9673191c1f5b8d9e927408ffc366
Abbey Rut <abbey.rut@gmail.com>
2022-12-01 12:57:12 +0200

    info to blog list

--- mind-matters/src/components/pages/BlogList.js
+++ mind-matters/src/components/pages/BlogList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const BlogList = () => {
-  return <div>BlogList info</div>;
+  return <div>BlogList info to this page
+    <h2>compant info</h2>
+  </div>;
 };
 
 export default BlogList;

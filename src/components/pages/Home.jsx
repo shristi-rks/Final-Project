1ff4276c11271aae266df4cbf340afb5c9bc5b97
shristi-rks <shristi.n08@gmail.com>
2022-12-06 20:47:04 +0200

    add bootstrap button in home component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import Button from '../Button'
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
     <>
-    <div className='page1'>
-      
+    <div className='page1 d-flex justify-content-evenly'>
+      <div>
       <h1>We’ve been there.<br/>We know how it feels.</h1>
-      <h4>Get matched with someone in our network that has<br/> already been through what you are going through.</h4>
+      <h4>Get matched with someone in our network that has
+        <br/> already been through what you are going through.</h4>
       </div>
       <div >
-    <Button/>
+      <Button href = './FormHelper' variant="primary" size="sm">
+        Register as a Helper
+        </Button> 
     </div>
+     </div>
+     
    
     </>
   )
